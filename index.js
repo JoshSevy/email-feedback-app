98f7edbe045dcec30bcc6854d708859c93fe45c7
JoshSevy <joshuasevy@outlook.com>
2020-11-19 02:28:40 -0700

    Add mondodb to api

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 require('./services/passport');
 
-
 const app = express();
 
 require('./routes/authRoutes')(app);
 
+mongoose.connect(key.mongoURI)
+
 const PORT = process.env.PORT||5000;
 app.listen(PORT); 
+
+
+
